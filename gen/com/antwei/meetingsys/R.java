2cc142fcce1f7faa083e1d94aaec67d193848782
simpleant <0antwei@gmail.com>
2013-10-17 15:00:41 +0800

    new commit

--- gen/com/antwei/meetingsys/R.java
+++ gen/com/antwei/meetingsys/R.java
@@ -28,6 +28,8 @@ public final class R {
         public static final int user_checkin=0x7f020004;
         public static final int user_uncheck=0x7f020005;
         public static final int wallpaper=0x7f020006;
+        public static final int wallpaperflip=0x7f020007;
+        public static final int wallpapertree=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f080017;
